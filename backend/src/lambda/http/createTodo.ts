a69006cdebf6d174c94790f65c98fdb8def11973
imsohuy <nguyenhuuhuy203@gmail.com>
2023-10-15 23:50:23 +0700

    init repo

---
+++ backend/src/lambda/http/createTodo.ts
@@ -0,0 +1,31 @@
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
+
+import { createTodo } from '../../businessLogic/todos'
+import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import { getUserId } from '../utils'
+
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const newTodo: CreateTodoRequest = JSON.parse(event.body)
+    
+    const userId = getUserId(event);
+    const todo = createTodo(userId, newTodo)
+
+    return {
+      statusCode: 201,
+      body: JSON.stringify({
+        item: todo
+      })
+    }
+  }
+)
+
+handler.use(
+  cors({
+    credentials: true
+  })
+)
